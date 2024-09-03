c29bcc262998cb795ed9c5ea303837cf77b1694d
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-09-03 07:49:06 +0530

    Create Student.java

---
+++ Student Management System/Student.java
@@ -0,0 +1,48 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private String rollNumber;
+    private String grade;
+
+    // Constructor
+    public Student(String name, String rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNumber='" + rollNumber + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
